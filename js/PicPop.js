9968d23a7bc5842d34cd54ffbe87450569edbfd2
J.J <jiajingloh@gmail.com>
2019-12-04 12:09:37 -0800

    new commit

---
+++ js/PicPop.js
@@ -0,0 +1,45 @@
+// AJAX weather
+
+
+
+$(document).ready(function(){
+$('#submitCity').on('focus', function(){
+
+const $minTemp = $('#weather-info #min-temperature');
+const $forecast = $('#forecast');
+const city = 'Houston'
+
+if(city != ''){
+    $.ajax({
+    url:"https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric" + "&APPID=7e97ca944b6826492b669fe353f1ba73",
+    type: "GET",
+    dataType: "jsonp",
+    success: function(data){
+      const weather = show(data);
+      $('#forecast').html(weather);
+      }
+    });
+  }
+});//submitcity button
+});// ready function
+
+
+
+function show(data){
+  const x = data.list[1].weather[0].main;
+
+  if (x == 'Clouds'){
+    $('#forecast').html("<img src='image/hiking.jfif'>");
+    }
+  else if(x == 'Clear'){
+       $('#forecast').html("<img src='image/hiking.jfif'>");
+       }
+  else if(x == 'Rain'){
+       $('#forecast').html("<img src='image/FlyingFishing.jpg'>");
+        }
+ else if (x == 'Snow'){
+            $('#forecast').html("<img src='imageSnowHiking.jpg'>");
+            }else{
+                $("#error").html('Field cannot be empty');
+              }
+};
